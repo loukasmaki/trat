a1c7090a6dd22037390206127e8b86bb66bbb57c
Petri Loukasmäki <petri@loukasmaki.se>
2020-05-31 00:10:51 +0200

    Trying to figure out git

---
+++ models.py
@@ -0,0 +1,37 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+db = SQLAlchemy()
+
+attendees_trainingday = db.Table('attendees_trainingday',
+    #db.Column('attendee_id', db.Integer, db.ForeignKey('attendee.id'), primary_key=True),
+    #db.Column('training_day_id', db.Integer, db.ForeignKey('training_day.id'), primary_key=True)
+    db.Column('attendee_id', db.Integer, db.ForeignKey('attendee.id'), primary_key=False),
+    db.Column('training_day_id', db.Integer, db.ForeignKey('training_day.id'), primary_key=False)
+    )
+
+class Attendee(db.Model):
+    __tablename__ = "attendee"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True, nullable=False)
+    training_dates = db.relationship('Training_day', secondary=attendees_trainingday, back_populates='attendees', lazy='subquery')
+
+class Training_day(db.Model):
+    __tablename__ = "training_day"
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.DateTime, nullable=False)
+    weapon = db.Column(db.String(30), nullable=False)
+    instructor = db.Column(db.String(50), server_default='ingen' )
+    attendees = db.relationship('Attendee',  secondary=attendees_trainingday, back_populates='training_dates', lazy='subquery')
+
+class User(db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(200))
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, server_default=db.func.now(), server_onupdate=db.func.now())
+    role = db.Column(db.String(20), server_default='member')
